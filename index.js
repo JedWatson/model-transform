3044373e1e51827dde1a1153a27badcaa64bd006
Jed Watson <jed.watson@me.com>
2015-06-13 23:59:20 +1000

    Export named function

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-function modelToJSON(schema, transform) {
+function modelToJSON(schema, fn) {
 	schema.set('toJSON', { transform: function(doc, rtn, options) {
 		rtn.id = rtn._id;
 		delete rtn._id;
 		delete rtn.__v;
-		if (transform) {
-			var result = transform(doc, rtn, options);
+		if (fn) {
+			var result = fn(doc, rtn, options);
 			if (result) rtn = result;
 		}
 		return rtn;
 	}});
 }
 
-module.exports = modelToJSON;
+module.toJSON = modelToJSON;
