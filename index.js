2959f8ce3fada01f63e29df5d1ccd142db8f2e74
Jed Watson <jed.watson@me.com>
2015-07-13 21:09:28 +1000

    Support for Keystone List schemas

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function modelToJSON(schema, fn) {
+	// support KeystoneJS Lists, which have the schema property nested
+	schema = schema.schema || schema;
 	schema.set('toJSON', { transform: function(doc, rtn, options) {
 		rtn.id = rtn._id;
 		delete rtn._id;
